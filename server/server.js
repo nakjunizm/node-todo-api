c05d1587f8b685b6e24d1d88896634b93570acae
n_j.sung <n_j.sung@samsung.com>
2018-01-26 14:48:17 +0900

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -114,6 +115,22 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+//POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+
+  let email = req.body.email;
+  let password = req.body.password;
+
+  User.findByCredentials(email, password).then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).status(200).send({user});
+      });
+    }).catch((e) => {
+      res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
