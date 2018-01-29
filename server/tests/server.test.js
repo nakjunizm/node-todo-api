18002b7dd69565158e0e3ac26b805f703851ce46
n_j.sung <n_j.sung@samsung.com>
2018-01-29 13:34:46 +0900

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -262,12 +262,32 @@ describe('POST /users/login', () => {
         if (err) {
           return done(err);
         }
-
         User.findById(users[1]._id).then((user) => {
           expect(user.tokens.length).toBe(0);
           done();
         }).catch((e) => done(e));
       });
 
+  });
+});
+
+describe('DELETE /users/me/token', () => {
+  it ('shoud remove auth token on logout', (done) => {
+    //DELETE /users/me/token
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[0]._id)
+          .then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e) => done(e));
+      });
   });
 });
